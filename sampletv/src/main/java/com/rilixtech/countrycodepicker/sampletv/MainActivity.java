79a6c2d7d0bae35c96b0148182e99587fd85a8b6
elshanalmasli <elshan.almasli@madappgang.com>
2023-10-18 16:48:39 +0400

    ccp from another lib moved here and try to publishing new build in jitpack

--- sampletv/src/main/java/com/rilixtech/countrycodepicker/sampletv/MainActivity.java
+++ sampletv/src/main/java/com/rilixtech/countrycodepicker/sampletv/MainActivity.java
@@ -3,7 +3,6 @@ package com.rilixtech.countrycodepicker.sampletv;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
-import com.rilixtech.widget.countrycodepicker.CountryCodePicker;
 
 public class MainActivity extends Activity {
 
@@ -12,9 +11,10 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    CountryCodePicker ccp = findViewById(R.id.ccp);
+    com.hbb20.CountryCodePicker
+            ccp = findViewById(R.id.ccp);
     EditText edtPhone = findViewById(R.id.phone_number_edt);
 
-    ccp.registerPhoneNumberTextView(edtPhone);
+    ccp.registerCarrierNumberEditText(edtPhone);
   }
 }
